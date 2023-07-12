0477c1ba7e8138b3ece5d9e6e1c3a63cd7255c8a
Daniel Feitosa <danielfeitosa.gama@outlook.com>
2023-07-12 15:21:24 -0300

    curso angular

---
+++ calculadora/src/app/calculadora/components/calculadora.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { CalculadoraService } from '../services';
+
+@Component({
+  selector: 'app-calculadora',
+  templateUrl: './calculadora.component.html',
+  styleUrls: ['./calculadora.component.css']
+})
+export class CalculadoraComponent{
+
+  constructor(private CalculadoraService: CalculadoraService) {
+
+  }
+
+}
